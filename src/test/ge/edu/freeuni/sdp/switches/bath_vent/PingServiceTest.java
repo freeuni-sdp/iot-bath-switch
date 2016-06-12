3d479d1f9616e2a451eea3ac8d32a01d53e132b7
khrak <mamuka_sakhelashvili@yahoo.com>
2016-06-12 18:17:10 +0400

    back

---
+++ src/test/ge/edu/freeuni/sdp/switches/bath_vent/PingServiceTest.java
@@ -0,0 +1,26 @@
+package ge.edu.freeuni.sdp.switches.bath_vent;
+
+import ge.edu.freeuni.sdp.iot.switches.bath_vent.PingService;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import static org.junit.Assert.*;
+
+/**
+ * Created by mi.maghriani on 6/11/2016.
+ */
+public class PingServiceTest extends JerseyTest{
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(PingService.class);
+    }
+
+    @Test
+    public void get() throws Exception {
+        Response response = target("ping").request().get();
+        assertEquals(200, response.getStatus());
+    }
+}
