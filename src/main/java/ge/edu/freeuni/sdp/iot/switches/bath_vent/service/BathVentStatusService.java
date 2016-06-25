5747235ebafff3f7b2410db1537eab0005e131a9
maghriany <mmagh12@freeuni.edu.ge>
2016-06-25 21:05:16 +0400

    added request handling

--- src/main/java/ge/edu/freeuni/sdp/iot/switches/bath_vent/service/BathVentStatusService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/switches/bath_vent/service/BathVentStatusService.java
@@ -1,9 +1,14 @@
 package ge.edu.freeuni.sdp.iot.switches.bath_vent.service;
 
+import ge.edu.freeuni.sdp.iot.switches.bath_vent.data.HomeData;
+import ge.edu.freeuni.sdp.iot.switches.bath_vent.model.Home;
 import ge.edu.freeuni.sdp.iot.switches.bath_vent.model.SwitchResponse;
 
 import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by khrak on 6/25/16.
@@ -16,7 +21,25 @@ public class BathVentStatusService {
     @GET
     public SwitchResponse get(@PathParam("house_id") String houseid) {
 
-        SwitchResponse switchResponse = new SwitchResponse("1234", "on", false);
+        Client client = ClientBuilder.newClient();
+        String houseID = "c637a8eb-5c1e-46fc-9de8-607df60df27a";
+        String url = "https://2.2.2.4" +
+                "/webapi/houses/" + houseID;
+
+        Response response =
+                client.target("")
+                        .request(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .get();
+
+        SwitchResponse switchResponse = response.readEntity(SwitchResponse.class);
+
+        Home home = HomeData.getInstance().getHome(houseid);
+
+        home.getVentSwitch().setStatus(switchResponse.getStatus());
+
         return switchResponse;
     }
+
+
 }
