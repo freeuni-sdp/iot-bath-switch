cc1886332b244db8fc9d61c9d24cc7de1ef37ca9
maghriany <mmagh12@freeuni.edu.ge>
2016-07-12 22:23:20 +0400

    added tests

--- src/main/java/ge/edu/freeuni/sdp/iot/switches/bath_vent/service/BathVentSwitchService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/switches/bath_vent/service/BathVentSwitchService.java
@@ -23,10 +23,63 @@ import javax.ws.rs.core.Response;
 @Produces( { MediaType.APPLICATION_JSON})
 public class BathVentSwitchService {
 
+    @GET
+    public Response get(@PathParam("house_id") String houseid) {
+
+
+        /* Take home data, where we'll find home identified by houseid */
+        HomeData homeData = HomeData.getInstance();
+
+
+        Home home = homeData.getHome(houseid);
+
+        /* if nothing was found, that return 404 error code */
+        if(home == null) {
+            SwitchResponse response = new SwitchResponse(houseid, null, false);
+            return Response.status(Response.Status.NOT_FOUND).entity(response).build();
+        }
+
+        Client client = ClientBuilder.newClient();
+
+        String url = home.getVentUrl() + "webapi/bath/vent-switch";
+
+
+        /* try to read current switch state. */
+        Response simulatorResponse =
+                client.target(url)
+                        .request(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .get();
+
+        if(simulatorResponse.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode()) {
+            /* if connection was failed, return result, according to last update. */
+            VentSwitch ventSwitch = home.getVentSwitch();
+            String status =  ventSwitch.getStatus();
+
+            SwitchResponse response = new SwitchResponse(houseid, status, false);
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(response).build();
+        }
+
+        String st = simulatorResponse.readEntity(String.class);
+
+        JSONObject jsonResponse = new JSONObject(st);
+
+        SwitchResponse switchResponse = new SwitchResponse(jsonResponse.getString("houseid"),
+                jsonResponse.getString("status"),
+                true);
+
+        /* keep current current response, got from simulator, in homeData. */
+        home.setVentSwitch(new VentSwitch(switchResponse.getHouseid(), switchResponse.getStatus()));
+
+        return Response.status(Response.Status.OK).entity(switchResponse).build();
+    }
+
+
     @POST
     public Response post(@PathParam("house_id") String houseid,
-                                String body) {
+                         String body) {
 
+        System.out.println("house id is " + houseid);
         HomeData homeData = HomeData.getInstance();
 
         Home home = homeData.getHome(houseid);
